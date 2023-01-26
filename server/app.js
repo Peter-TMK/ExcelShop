3ba3cde8fb7a9f85a98e258569d3b699b6d45435
Peter-TMK <tundepeter01@gmail.com>
2023-01-26 20:47:14 +0100

    server setup

--- server/app.js
+++ server/app.js
@@ -1,1 +1,14 @@
-console.log('Hello, Excel Shop')
+const express = require('express');
+const app = express();
+// require('dotenv').config()
+require('dotenv/config')
+const api = process.env.API_URL
+const PORT = process.env.PORT
+
+app.get(api+'/products', (req, res)=>{
+    res.send('Hello ExcelShop!')
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
