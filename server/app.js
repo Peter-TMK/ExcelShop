266ca9b2c990d5a7ef96314d46edb19ff086e167
Peter-TMK <tundepeter01@gmail.com>
2023-01-27 11:47:00 +0100

    new: persisting data to mongoDB

--- server/app.js
+++ server/app.js
@@ -7,36 +7,61 @@ const mongoose = require('mongoose')
 app.use(express.json())
 app.use(morgan('tiny'))
 
+const ProductSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number,
+})
+
+const Product = mongoose.model('Product', ProductSchema)
+
 // require('dotenv').config()
 require('dotenv/config')
 const api = process.env.API_URL
 const PORT = process.env.PORT
 
-app.get(`${api}/products`, (req, res) => {
-    const products = {
-        id: 1,
-        prodName: 'Laptop',
-        qty: 5,
+app.get(`${api}/products`, async (req, res) => {
+    const productItem = await Product.find()
+
+    if (!productItem) {
+        res.status(500).json({
+            error: 'No product found!',
+        })
     }
-    res.send(products)
+    res.send(productItem)
 })
 
 app.post(`${api}/products`, (req, res) => {
-    const productName = req.body
-    console.log(productName)
-    res.send(productName)
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock,
+    })
+
+    product
+        .save()
+        .then((createdProduct) => {
+            res.status(201).json(createdProduct)
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err,
+                success: false,
+            })
+        })
 })
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('Database connected successfully!')
-})
-.catch((err) => {
-    console.log(err)
-})
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Database connected successfully!')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
