7590adbec27d0c9c524b6b300d339e788e4c9dd1
Peter-TMK <tundepeter01@gmail.com>
2023-01-29 18:25:55 +0100

    update: added more CRUD functionalities to the category route

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -14,16 +14,41 @@ categoryRouter.get('/', async (req, res) => {
     res.send(categoryItem)
 })
 
+categoryRouter.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id)
+    if (!category) {
+        res.status(500).json({
+            error: 'No such category with ID found!',
+        })
+    }
+    res.status(200).send(category)
+})
+
 categoryRouter.post('/', async (req, res) => {
     const category = new Category({
         name: req.body.name,
         icon: req.body.icon,
         color: req.body.color,
     })
-    
     await category.save();
     if(!category){
         return res.status(404).send('Category not created!')
+    }
+    res.status(201).send(category);
+})
+
+categoryRouter.put('/:id', async (req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color,
+        },
+        {new: true}
+    )
+    if(!category){
+        return res.status(400).send('Category cannot be updated!')
     }
     res.status(201).send(category);
 })
